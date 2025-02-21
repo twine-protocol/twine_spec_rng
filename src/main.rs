711da6eb94fcc816f4c7437fc8fd175ea4053b41
Jasper Palfree <well.caffeinated@gmail.com>
2025-02-20 17:10:44 -0700

    added quick binary to check randomness from an http api

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use twine::prelude::Resolver;
+use twine::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  // first arg: url to api
+  // second arg: twine query to rng tixel
+  let args: Vec<String> = std::env::args().collect();
+  let url = &args[1];
+  let query = &args[2];
+
+  let client = twine_http_store::reqwest::Client::new();
+  let store = twine_http_store::v2::HttpStore::new(client)
+    .with_url(url);
+
+  let query: SingleQuery = query.parse()?;
+  let result = store.resolve(query).await?;
+  let current = result.unpack();
+  let prev = store.resolve(current.previous().unwrap()).await?;
+
+  let rand = twine_spec_rng::extract_randomness(&current, &prev)?;
+
+  // print as hex string
+  println!("Successfully extracted randomness:");
+  println!("{}", hex::encode(rand));
+
+  Ok(())
+}
